98d7901938473924bae47edc74a69b6659a1803b
Yohanito Leão <yohan.leao@gmail.com>
2016-06-25 13:33:03 +0200

    Header + CSS + Index2(testfile)

---
+++ website/index2.php
@@ -0,0 +1,12 @@
+<?php include('header.php');?>
+    <section id="about" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+            	Voiture: 0 ou 1<br>Prix: entre 100 et 3097<br><br>
+            	<?php include('search.php');/*
+            	Ancienne recherche :
+            	 include('anciennerecherche.php');*/?>
+            </div>
+        </div>
+    </section>
+<?php include('footer.php');?>
