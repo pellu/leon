2c4bb7b584de416398b70ca5237b0d26d9e1f8c4
Yohanito Leão <yohan.leao@gmail.com>
2016-06-27 23:45:18 +0200

    HTML New HTML + Continue CSS + IMG

--- website/index.php
+++ website/index.php
@@ -1,85 +1,88 @@
 <?php include('header.php'); ?>
-    <section class="container content-section text-center">
-        <div class="row">
-            <div class="col-lg-12 col-ls-12 col-xs-12" id="parallax">
-                <h1 id="maintitle">The Playce to be</h1>
-                <h2 id="baseline">Trouvez votre soir&eacute;e sans plus attendre !</h2>
+<section class="container content-section text-center">
+    <div class="row">
+        <div class="col-lg-12 col-ls-12 col-xs-12 parallax" id="trans1">
+            <h1 id="maintitle">The Playce to be</h1>
+            <h2 id="baseline">Trouvez votre soir&eacute;e sans plus attendre !</h2>
+            <div id="opacitysquare" class="col-lg-12 col-ls-12 col-xs-12">
                 <div class="col-lg-8 col-ls-12 col-xs-12 col-lg-offset-2" id="homesearch">
                     <form action="search.php" method="POST">
-                      <select name="search">
-                        <option value="" disabled selected>Où vas-tu jouer ?</option>
-                        <option value="antony">Antony (92)</option>
-                        <option value="argenteuil">Argenteuil (95)</option>
-                        <option value="aubervilliers">Aubervilliers (93)</option>
-                        <option value="aulnay-sous-bois">Aulnay-sous-Bois (93)</option>
-                        <option value="asnieres-sur-seine">Asni&egrave;res-sur-Seine (92)</option>
-                        <option value="Boulogne-Billancourt">Boulogne-Billancourt (92)</option>
-                        <option value="bondy">Bondy (93)</option>
-                        <option value="cergy">Cergy (95)</option>
-                        <option value="champigny-sur-marne">Champigny-sur-Marne (94)</option>
-                        <option value="chelles">Chelles (77)</option>
-                        <option value="clamart">Clamart (92)</option>
-                        <option value="clichy">Clichy (92)</option>
-                        <option value="colombes">Colombes (92)</option>
-                        <option value="courbevoie">Courbevoie (92)</option>
-                        <option value="creteil">Cr&eacute;teil (94)</option>
-                        <option value="drancy">Drancy (93)</option>
-                        <option value="epinay-sur-seine">&Eacute;pinay-sur-Seine (93)</option>
-                        <option value="evry">&Eacute;vry (91)</option>
-                        <option value="fontenay-sous-Bois">Fontenay-sous-Bois (94)</option>
-                        <option value="issy-les-moulineaux">Issy-les-Moulineaux (92)</option>
-                        <option value="ivry-sur-seine">Ivry-sur-Seine (94)</option>
-                        <option value="le-blanc-mesnil">Le Blanc-Mesnil (93)</option>
-                        <option value="levallois-perret">Levallois-Perret (92)</option>
-                        <option value="maisons-alfort">Maisons-Alfort (94)</option>
-                        <option value="meaux">Meaux (77)</option>
-                        <option value="montreuil">Montreuil (93)</option>
-                        <option value="nanterre">Nanterre (92)</option>
-                        <option value="paris1">Neuilly-sur-Seine (92)</option>
-                        <option value="noisy-le-grand">Noisy-le-Grand (93)</option>
-                        <option value="pantin">Pantin (93)</option>
-                        <option value="paris1">Paris (75 001)</option>
-                        <option value="paris1">Paris (75 002)</option>
-                        <option value="paris3">Paris (75 003)</option>
-                        <option value="paris4">Paris (75 004)</option>
-                        <option value="paris5">Paris (75 005)</option>
-                        <option value="paris6">Paris (75 006)</option>
-                        <option value="paris7">Paris (75 007)</option>
-                        <option value="paris8">Paris (75 008)</option>
-                        <option value="paris9">Paris (75 009)</option>
-                        <option value="paris10">Paris (75 010)</option>
-                        <option value="paris11">Paris (75 011)</option>
-                        <option value="paris12">Paris (75 012)</option>
-                        <option value="paris13">Paris (75 013)</option>
-                        <option value="paris14">Paris (75 014)</option>
-                        <option value="paris15">Paris (75 015)</option>
-                        <option value="paris16">Paris (75 016)</option>
-                        <option value="paris17">Paris (75 017)</option>
-                        <option value="paris18">Paris (75 018)</option>
-                        <option value="paris19">Paris (75 019)</option>
-                        <option value="paris20">Paris (75 020)</option>
-                        <option value="rueil-malmaison">Rueil-Malmaison (92)</option>
-                        <option value="saint-denis">Saint-Denis (93)</option>
-                        <option value="saint-maur-des-fosses">Saint-Maur-des-Foss&eacute;s (94)</option>
-                        <option value="sarcelles">Sarcelles (95)</option>
-                        <option value="sartrouville">Sartrouville (78)</option>
-                        <option value="sevran">Sevran (93)</option>
-                        <option value="versailles">Versailles (78)</option>
-                        <option value="villejuif">Villejuif (94)</option>
-                        <option value="vitry-sur-seine">Vitry-sur-Seine (94)</option>
-                      </select>
-                    <input type="submit" value="Recherche" name="submit">
-                  </form>
+                        <select name="search">
+                            <option value="" disabled selected>Où vas-tu jouer ?</option>
+                            <option value="antony">Antony (92)</option>
+                            <option value="argenteuil">Argenteuil (95)</option>
+                            <option value="aubervilliers">Aubervilliers (93)</option>
+                            <option value="aulnay-sous-bois">Aulnay-sous-Bois (93)</option>
+                            <option value="asnieres-sur-seine">Asni&egrave;res-sur-Seine (92)</option>
+                            <option value="Boulogne-Billancourt">Boulogne-Billancourt (92)</option>
+                            <option value="bondy">Bondy (93)</option>
+                            <option value="cergy">Cergy (95)</option>
+                            <option value="champigny-sur-marne">Champigny-sur-Marne (94)</option>
+                            <option value="chelles">Chelles (77)</option>
+                            <option value="clamart">Clamart (92)</option>
+                            <option value="clichy">Clichy (92)</option>
+                            <option value="colombes">Colombes (92)</option>
+                            <option value="courbevoie">Courbevoie (92)</option>
+                            <option value="creteil">Cr&eacute;teil (94)</option>
+                            <option value="drancy">Drancy (93)</option>
+                            <option value="epinay-sur-seine">&Eacute;pinay-sur-Seine (93)</option>
+                            <option value="evry">&Eacute;vry (91)</option>
+                            <option value="fontenay-sous-Bois">Fontenay-sous-Bois (94)</option>
+                            <option value="issy-les-moulineaux">Issy-les-Moulineaux (92)</option>
+                            <option value="ivry-sur-seine">Ivry-sur-Seine (94)</option>
+                            <option value="le-blanc-mesnil">Le Blanc-Mesnil (93)</option>
+                            <option value="levallois-perret">Levallois-Perret (92)</option>
+                            <option value="maisons-alfort">Maisons-Alfort (94)</option>
+                            <option value="meaux">Meaux (77)</option>
+                            <option value="montreuil">Montreuil (93)</option>
+                            <option value="nanterre">Nanterre (92)</option>
+                            <option value="paris1">Neuilly-sur-Seine (92)</option>
+                            <option value="noisy-le-grand">Noisy-le-Grand (93)</option>
+                            <option value="pantin">Pantin (93)</option>
+                            <option value="paris1">Paris (75 001)</option>
+                            <option value="paris1">Paris (75 002)</option>
+                            <option value="paris3">Paris (75 003)</option>
+                            <option value="paris4">Paris (75 004)</option>
+                            <option value="paris5">Paris (75 005)</option>
+                            <option value="paris6">Paris (75 006)</option>
+                            <option value="paris7">Paris (75 007)</option>
+                            <option value="paris8">Paris (75 008)</option>
+                            <option value="paris9">Paris (75 009)</option>
+                            <option value="paris10">Paris (75 010)</option>
+                            <option value="paris11">Paris (75 011)</option>
+                            <option value="paris12">Paris (75 012)</option>
+                            <option value="paris13">Paris (75 013)</option>
+                            <option value="paris14">Paris (75 014)</option>
+                            <option value="paris15">Paris (75 015)</option>
+                            <option value="paris16">Paris (75 016)</option>
+                            <option value="paris17">Paris (75 017)</option>
+                            <option value="paris18">Paris (75 018)</option>
+                            <option value="paris19">Paris (75 019)</option>
+                            <option value="paris20">Paris (75 020)</option>
+                            <option value="rueil-malmaison">Rueil-Malmaison (92)</option>
+                            <option value="saint-denis">Saint-Denis (93)</option>
+                            <option value="saint-maur-des-fosses">Saint-Maur-des-Foss&eacute;s (94)</option>
+                            <option value="sarcelles">Sarcelles (95)</option>
+                            <option value="sartrouville">Sartrouville (78)</option>
+                            <option value="sevran">Sevran (93)</option>
+                            <option value="versailles">Versailles (78)</option>
+                            <option value="villejuif">Villejuif (94)</option>
+                            <option value="vitry-sur-seine">Vitry-sur-Seine (94)</option>
+                        </select>
+                        <input type="submit" value="Recherche" name="submit">
+                    </form>
                 </div>
-                <div class="col-lg-8 col-ls-12 col-xs-12 col-lg-offset-2" id="forcescroll">
-                    <h3>Decouvrir Playce</h3>
+            </div>
+        </div>
+    </div>
+    <div id="rest" class="col-lg-12 col-ls-12 col-xs-12">
+        <div class="col-lg-8 col-ls-12 col-xs-12 col-lg-offset-2" id="forcescroll">
+            <h3>Decouvrir Playce</h3>
                 <span class="scroll-btn glowing-btn">
                     <a href="#">
                         <span class="mouse"><span></span></span>
                     </a>
                 </span>
-                </div>
-            </div>
         </div>
         <div class="col-lg-6 col-ls-6 col-xs-12 col-lg-offset-3 col-ls-offset-3">
             <h3 id="h3">Qu'est-ce que Playce ?</h3>
@@ -113,7 +116,7 @@
         <div class="col-lg-6 col-ls-6 col-xs-12 col-lg-offset-3 col-ls-offset-3">
             <h3 class="glowingtext">Maintenant,<br/>Choisissez votre camp !</h3>
         </div>
-        <div class="col-lg-8 col-ls-8 col-xs-12 col-lg-offset-2 col-ls-offset-2">
+        <div class="col-lg-8 col-ls-8 col-xs-12 col-lg-offset-3 col-ls-offset-3">
             <a href="#">
                 <div class="col-lg-5 col-ls-5 col-xs-5 text-right" id="hote">Hote</div>
             </a>
@@ -131,14 +134,16 @@
             <div class="comment col-lg-8 col-ls-8 col-xs-8 text-left">
                 <p class="user-name">Lucas Bedouin, hôte</p>
                 <p class="user-comment">En ligne j'avais l'impression de jouer avec des robots, ou en tout cas pas avec
-                    de "vraies" personnes. Le gros plus de Playce est sans conteste sa dimension humaine et chaleureuse.</p>
+                    de "vraies" personnes. Le gros plus de Playce est sans conteste sa dimension humaine et
+                    chaleureuse.</p>
             </div>
         </div>
         <div class="comment-left col-lg-6 col-ls-6 col-xs-12">
             <div class="comment col-lg-8 col-ls-8 col-xs-8 text-right">
                 <p class="user-name">Lucas Bedouin, hôte</p>
                 <p class="user-comment">En ligne j'avais l'impression de jouer avec des robots, ou en tout cas pas avec
-                    de "vraies" personnes. Le gros plus de Playce est sans conteste sa dimension humaine et chaleureuse.</p>
+                    de "vraies" personnes. Le gros plus de Playce est sans conteste sa dimension humaine et
+                    chaleureuse.</p>
             </div>
             <div class="user-avatar col-lg-4 col-ls-4 col-xs-4">
                 <img src="#" alt="Photo de profil">
@@ -151,14 +156,16 @@
             <div class="comment col-lg-8 col-ls-8 col-xs-8 text-left">
                 <p class="user-name">Lucas Bedouin, hôte</p>
                 <p class="user-comment">En ligne j'avais l'impression de jouer avec des robots, ou en tout cas pas avec
-                    de "vraies" personnes. Le gros plus de Playce est sans conteste sa dimension humaine et chaleureuse.</p>
+                    de "vraies" personnes. Le gros plus de Playce est sans conteste sa dimension humaine et
+                    chaleureuse.</p>
             </div>
         </div>
         <div class="comment-left col-lg-6 col-ls-6 col-xs-12">
             <div class="comment col-lg-8 col-ls-8 col-xs-8 text-right">
                 <p class="user-name">Lucas Bedouin, hôte</p>
                 <p class="user-comment">En ligne j'avais l'impression de jouer avec des robots, ou en tout cas pas avec
-                    de "vraies" personnes. Le gros plus de Playce est sans conteste sa dimension humaine et chaleureuse.</p>
+                    de "vraies" personnes. Le gros plus de Playce est sans conteste sa dimension humaine et
+                    chaleureuse.</p>
             </div>
             <div class="user-avatar col-lg-4 col-ls-4 col-xs-4">
                 <img src="#" alt="Photo de profil">
@@ -170,5 +177,50 @@
                                value="Partagez votre expérience">
                     </span>
         </div>
-    </section>
+    </div>
+</section>
 <?php include('footer.php'); ?>
+<script type="text/javascript">
+    (function ($) {
+
+        $.fn.parallax = function (options) {
+
+            var windowHeight = $(window).height();
+
+            // Establish default settings
+            var settings = $.extend({
+                speed: 0.15
+            }, options);
+
+            // Iterate over each object in collection
+            return this.each(function () {
+
+                // Save a reference to the element
+                var $this = $(this);
+
+                // Set up Scroll Handler
+                $(document).scroll(function () {
+
+                    var scrollTop = $(window).scrollTop();
+                    var offset = $this.offset().top;
+                    var height = $this.outerHeight();
+
+                    // Check if above or below viewport
+                    if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+                        return;
+                    }
+
+                    var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
+
+                    // Apply the Y Background Position to Set the Parallax Effect
+                    $this.css('background-position', 'center ' + yBgPosition + 'px');
+
+                });
+            });
+        }
+    }(jQuery));
+
+    $('.bg-1').parallax({
+        speed: 0.15
+    });
+</script>
