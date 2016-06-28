d6bfeea346db178ca5d13314d5fcc917d0d1cea8
pellu <jordanpelluard@gmail.com>
2016-06-28 19:05:53 +0200

    visu du pseudo au lieu du mail dans admin profil

--- website/monprofil.php
+++ website/monprofil.php
@@ -5,11 +5,13 @@
     <div class="col-lg-12 col-ls-12 col-xs-12">
     <br><br><br><br><br>
 <?php if(isset($_SESSION['email'])){?>
-<?php 
+<?php
 	include('headerprofil.php');
-	//Affichage du pseudo de l'user
-	echo'Bonjour, ' .$_SESSION['email']. '<br><br>';
-
+	$email=$_SESSION["email"];
+	$sqluser="SELECT * FROM profil WHERE email='".$email."'";
+	$requser = mysql_query($sqluser) or die('Erreur SQL !<br />'.$sqluser.'<br />'.mysql_error());
+	$datauser=mysql_fetch_assoc($requser);
+    echo 'Bonjour, ' .$datauser['pseudo']. '';
 }else{?>
 	<a href="deconnexion.php">Je me déconnecte</a> (Vous serez déconnecté à la fermeture de votre navigateur)
 <div class="message">Pour acc&eacute;der &agrave; cette page, vous devez &ecirc;tre connect&eacute;.<br />
