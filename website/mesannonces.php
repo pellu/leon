0f78a595921f4e624964f8256e55760c1e540b1f
Yohanito Leão <yohan.leao@gmail.com>
2016-07-01 16:30:42 +0200

    mes annonces v0.9 desktop

--- website/mesannonces.php
+++ website/mesannonces.php
@@ -3,21 +3,21 @@
 if (isset($_SESSION['email'])) {
     header('content-type: text/html; charset=ISO-8859-1');
     ?>
-    <section class="container-fluid content-section text-center">
+    <section class="container-fluid content-section text-center" xmlns="http://www.w3.org/1999/html">
         <div class="row">
             <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12' id='rest'>
-                <div class='col-lg-8 col-md-12 col-sm-12 col-xs-12 col-lg-offset-2'>
+                <div class='col-lg-10 col-md-12 col-sm-12 col-xs-12 col-lg-offset-1'>
                     <br><br><br><br><br>
                     <?php include('headerprofil.php');
-                    /* Remplace caractères accentués d'une chaine */
+                    /* Remplace caract&egrave;res accentu&eacute;s d'une chaine */
                     function remove_accent($str)
                     {
-                        $a = array('À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Æ', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'ß', 'à', 'á', 'â', 'ã', 'ä', 'å', 'æ', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ', 'Ā', 'ā', 'Ă', 'ă', 'Ą', 'ą', 'Ć', 'ć', 'Ĉ', 'ĉ', 'Ċ', 'ċ', 'Č', 'č', 'Ď', 'ď', 'Đ', 'đ', 'Ē', 'ē', 'Ĕ', 'ĕ', 'Ė', 'ė', 'Ę', 'ę', 'Ě', 'ě', 'Ĝ', 'ĝ', 'Ğ', 'ğ', 'Ġ', 'ġ', 'Ģ', 'ģ', 'Ĥ', 'ĥ', 'Ħ', 'ħ', 'Ĩ', 'ĩ', 'Ī', 'ī', 'Ĭ', 'ĭ', 'Į', 'į', 'İ', 'ı', 'Ĳ', 'ĳ', 'Ĵ', 'ĵ', 'Ķ', 'ķ', 'Ĺ', 'ĺ', 'Ļ', 'ļ', 'Ľ', 'ľ', 'Ŀ', 'ŀ', 'Ł', 'ł', 'Ń', 'ń', 'Ņ', 'ņ', 'Ň', 'ň', 'ŉ', 'Ō', 'ō', 'Ŏ', 'ŏ', 'Ő', 'ő', 'Œ', 'œ', 'Ŕ', 'ŕ', 'Ŗ', 'ŗ', 'Ř', 'ř', 'Ś', 'ś', 'Ŝ', 'ŝ', 'Ş', 'ş', 'Š', 'š', 'Ţ', 'ţ', 'Ť', 'ť', 'Ŧ', 'ŧ', 'Ũ', 'ũ', 'Ū', 'ū', 'Ŭ', 'ŭ', 'Ů', 'ů', 'Ű', 'ű', 'Ų', 'ų', 'Ŵ', 'ŵ', 'Ŷ', 'ŷ', 'Ÿ', 'Ź', 'ź', 'Ż', 'ż', 'Ž', 'ž', 'ſ', 'ƒ', 'Ơ', 'ơ', 'Ư', 'ư', 'Ǎ', 'ǎ', 'Ǐ', 'ǐ', 'Ǒ', 'ǒ', 'Ǔ', 'ǔ', 'Ǖ', 'ǖ', 'Ǘ', 'ǘ', 'Ǚ', 'ǚ', 'Ǜ', 'ǜ', 'Ǻ', 'ǻ', 'Ǽ', 'ǽ', 'Ǿ', 'ǿ');
+                        $a = array('À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Æ', 'Ç', '&egrave;', '&eacute;', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'ß', 'à', 'á', 'â', 'ã', 'ä', 'å', 'æ', 'ç', '&egrave;', '&eacute;', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ', 'Ā', 'ā', 'Ă', 'ă', 'Ą', 'ą', 'Ć', 'ć', 'Ĉ', 'ĉ', 'Ċ', 'ċ', 'Č', 'č', 'Ď', 'ď', 'Đ', 'đ', 'Ē', 'ē', 'Ĕ', 'ĕ', 'Ė', 'ė', 'Ę', 'ę', 'Ě', 'ě', 'Ĝ', 'ĝ', 'Ğ', 'ğ', 'Ġ', 'ġ', 'Ģ', 'ģ', 'Ĥ', 'ĥ', 'Ħ', 'ħ', 'Ĩ', 'ĩ', 'Ī', 'ī', 'Ĭ', 'ĭ', 'Į', 'į', 'İ', 'ı', 'Ĳ', 'ĳ', 'Ĵ', 'ĵ', 'Ķ', 'ķ', 'Ĺ', 'ĺ', 'Ļ', 'ļ', 'Ľ', 'ľ', 'Ŀ', 'ŀ', 'Ł', 'ł', 'Ń', 'ń', 'Ņ', 'ņ', 'Ň', 'ň', 'ŉ', 'Ō', 'ō', 'Ŏ', 'ŏ', 'Ő', 'ő', 'Œ', 'œ', 'Ŕ', 'ŕ', 'Ŗ', 'ŗ', 'Ř', 'ř', 'Ś', 'ś', 'Ŝ', 'ŝ', 'Ş', 'ş', 'Š', 'š', 'Ţ', 'ţ', 'Ť', 'ť', 'Ŧ', 'ŧ', 'Ũ', 'ũ', 'Ū', 'ū', 'Ŭ', 'ŭ', 'Ů', 'ů', 'Ű', 'ű', 'Ų', 'ų', 'Ŵ', 'ŵ', 'Ŷ', 'ŷ', 'Ÿ', 'Ź', 'ź', 'Ż', 'ż', 'Ž', 'ž', 'ſ', 'ƒ', 'Ơ', 'ơ', 'Ư', 'ư', 'Ǎ', 'ǎ', 'Ǐ', 'ǐ', 'Ǒ', 'ǒ', 'Ǔ', 'ǔ', 'Ǖ', 'ǖ', 'Ǘ', 'ǘ', 'Ǚ', 'ǚ', 'Ǜ', 'ǜ', 'Ǻ', 'ǻ', 'Ǽ', 'ǽ', 'Ǿ', 'ǿ');
                         $b = array('A', 'A', 'A', 'A', 'A', 'A', 'AE', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'D', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 's', 'a', 'a', 'a', 'a', 'a', 'a', 'ae', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'n', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y', 'A', 'a', 'A', 'a', 'A', 'a', 'C', 'c', 'C', 'c', 'C', 'c', 'C', 'c', 'D', 'd', 'D', 'd', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'G', 'g', 'G', 'g', 'G', 'g', 'G', 'g', 'H', 'h', 'H', 'h', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'IJ', 'ij', 'J', 'j', 'K', 'k', 'L', 'l', 'L', 'l', 'L', 'l', 'L', 'l', 'L', 'l', 'N', 'n', 'N', 'n', 'N', 'n', 'n', 'O', 'o', 'O', 'o', 'O', 'o', 'OE', 'oe', 'R', 'r', 'R', 'r', 'R', 'r', 'S', 's', 'S', 's', 'S', 's', 'S', 's', 'T', 't', 'T', 't', 'T', 't', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'W', 'w', 'Y', 'y', 'Y', 'Z', 'z', 'Z', 'z', 'Z', 'z', 's', 'f', 'O', 'o', 'U', 'u', 'A', 'a', 'I', 'i', 'O', 'o', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'A', 'a', 'AE', 'ae', 'O', 'o');
                         return str_replace($a, $b, $str);
                     }
 
-                    /* Générateur de Slug (Friendly Url) : convertit un titre en une URL conviviale.*/
+                    /* G&eacute;n&eacute;rateur de Slug (Friendly Url) : convertit un titre en une URL conviviale.*/
                     function Slug($str)
                     {
                         return mb_strtolower(preg_replace(array('/[^a-zA-Z0-9 \'-]/', '/[ -\']+/', '/^-|-$/'),
@@ -32,7 +32,7 @@ if (isset($_SESSION['email'])) {
                         <div
                             class="col-lg-3 col-md-3 col-sm-3 col-xs-12 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 events-bg-black">
                             <h2 id="h4">Mes annonces (4)</h2>
-                            <a href="http://localhost/leon/website/mesannonces.php">J'actualise mes annonces</a><br><br>
+                            <p><a href="http://localhost/leon/website/mesannonces.php" class="info-address">J'actualise mes annonces</a></p><br><br>
                             <?php
                             $id_annonce = $_SESSION["userid"];
 
@@ -49,11 +49,16 @@ if (isset($_SESSION['email'])) {
                             while ($resultnews = mysql_fetch_array($resnews)) {
                                 ?>
                                 <?php echo $resultnews['datedejeu']; ?><br>
-                                <a target="_blank"
-                                   href="http://localhost/leon/website/news/<?php echo $resultnews['url_news']; ?>-<?php echo $resultnews['id_news']; ?>"><?php echo $resultnews['titre_news']; ?></a>
-                                <br>
-                                <a href="http://localhost/leon/website/modifierannonce.php?id=<?php echo $resultnews['id_news']; ?>">Je
-                                    modifier mon annonce</a><br><br>
+                                <div class="item"><img
+                                        src="http://localhost/leon/website/photos/<?php echo $resultnews['photo']; ?>"
+                                        alt="">
+                                    <div class="overlay"><a
+                                            href="http://localhost/leon/website/modifierannonce.php?id=<?php echo $resultnews['id_news']; ?>">Modifier
+                                            l'annonce <i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></div>
+                                </div>
+                                <div class="squared-title"><a target="_blank"
+                                                              href="http://localhost/leon/website/news/<?php echo $resultnews['url_news']; ?>-<?php echo $resultnews['id_news']; ?>"><?php echo $resultnews['titre_news']; ?></a>
+                                </div>
                                 <?php
                             }
                             ?>
@@ -64,7 +69,7 @@ if (isset($_SESSION['email'])) {
 
                         // SCRIPT ENVOI PHOTO
                         define('TARGET', '../website/photos/');    // Repertoire cible
-                        define('MAX_SIZE', 100000);    // Taille max en octets du fichier
+                        define('MAX_SIZE', 10000000);    // Taille max en octets du fichier
                         define('WIDTH_MAX', 800);    // Largeur max de l'image en pixels
                         define('HEIGHT_MAX', 800);    // Hauteur max de l'image en pixels
 
@@ -82,7 +87,7 @@ if (isset($_SESSION['email'])) {
                          *************************************************************/
                         if (!is_dir(TARGET)) {
                             if (!mkdir(TARGET, 0755)) {
-                                exit('Erreur : le répertoire cible ne peut-être créé ! Vérifiez que vous diposiez des droits suffisants pour le faire ou créez le manuellement !');
+                                exit('Erreur : le r&eacute;pertoire cible ne peut-être cr&eacute;&eacute; ! V&eacute;rifiez que vous diposiez des droits suffisants pour le faire ou cr&eacute;ez le manuellement !');
                             }
                         }
 
@@ -110,13 +115,13 @@ if (isset($_SESSION['email'])) {
 
                                                 // Si c'est OK, on teste l'upload
                                                 if (move_uploaded_file($_FILES['photo']['tmp_name'], TARGET . $nomImage)) {
-                                                    $message = 'Upload réussi !';
+                                                    $message = 'Upload r&eacute;ussi !';
                                                 } else {
                                                     // Sinon on affiche une erreur systeme
-                                                    $message = 'Problème lors de l\'upload !';
+                                                    $message = 'Probl&egrave;me lors de l\'upload !';
                                                 }
                                             } else {
-                                                $message = 'Une erreur interne a empêché l\'uplaod de l\'image';
+                                                $message = 'Une erreur interne a empêch&eacute; l\'uplaod de l\'image';
                                             }
                                         } else {
                                             // Sinon erreur sur les dimensions et taille de l'image
@@ -243,12 +248,11 @@ if (isset($_SESSION['email'])) {
                                 $stmt->execute();
                                 $titre_news = '';
                                 $typedejeu = '';
-                                $adresse_news = '';
                                 $contenu_news = '';
                                 $titre_news = '';
                                 $datedejeu = '';
                                 $heuredejeu = '';
-                                $champspasremplis = 'L\'anonce est bien envoy&eacute;e<br><br>';
+                                $champspasremplis = '<p>L\'annonce est bien envoy&eacute;e<br><br></p>';
                             }
                         }
                         ?>
@@ -352,8 +356,9 @@ if (isset($_SESSION['email'])) {
                                     </select>
                                 </fieldset>
                                 <fieldset>
-                                    <label>Description</label><input type="text" name="contenu_news"
-                                                                     value="<?php echo $contenu_news ?>"/>
+                                    <label>Description</label><textarea " name="contenu_news"
+                                                                     id="annonce-description" placeholder="D&eacute;crivez le th&egrave;me et le d&eacute;roulement  de votre soir&eacute;e…"
+                                                                     value="<?php echo $contenu_news ?>"/></textarea>
                                 </fieldset>
                                 <fieldset>
                                     <label>Date de la soir&eacute;e</label><input type="date" name="datedejeu"
@@ -365,9 +370,10 @@ if (isset($_SESSION['email'])) {
                                 </fieldset>
                                 <fieldset>
                                     <input type="hidden" name="url_news" value="<?php echo $url_news ?>"/>
-                                    <div class="checkbox"><input type="checkbox"  name="choix[]" value="1"></div> <a target="_blank"
-                                                                                        href="conditions.php" class="info-address">J'accepte
-                                        les conditions g&eacute;n&eacute;rales d'utilisation</a>
+                                    <label> J'accepte
+                                        les <a target="_blank" href="conditions.php" id="cgu">CGU</a></label><input
+                                        type="checkbox" class="right" name="choix[]"
+                                        value="1">
                                 </fieldset>
 
                                 <input type="submit" name="submit" value="ENVOYER"/>
