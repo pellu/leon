a6db696cc25a44d08e4a199bf0c2822c82a51ee8
Yohanito Leão <yohan.leao@gmail.com>
2016-07-05 18:50:59 +0200

    motdepasseoublie.php responsive

--- website/motdepasseoublie.php
+++ website/motdepasseoublie.php
@@ -1,27 +1,39 @@
-<?php session_start();?>
-<?php include('menu.php');?>
-<br><br><br><br><br><br><br><br>
-<?php
-if(isset($_POST['email'])){
-  $email=$_POST['email'];
-}else{$email="";}
+<?php session_start(); ?>
+<?php include('menu.php'); ?>
+    <section id="rest" class="container-fluid content-section text-center">
+        <div class="row">
+            <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+                <div class='col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-12 col-xs-12' id="formulaire">
+                    <br><br><br><br><br><br>
+                    <?php
+                    if (isset($_POST['email'])) {
+                        $email = $_POST['email'];
+                    } else {
+                        $email = "";
+                    }
 
-if (isset($_POST['envoi']) AND ($_POST['email'])){
-	echo 'Vous recevrez un mail avec un lien';
-}
-else{
-	// afficher le formulaire
-	echo '
+                    if (isset($_POST['envoi']) AND ($_POST['email'])) {
+                        echo 'Vous recevrez un mail avec un lien';
+                    } else {
+                        // afficher le formulaire
+                        echo '
 		<form method="post" action="">
-				<div class="input-group col-lg-12 contact-form">
-				<label class="col-lg-4 col-md-4 col-sm-4 col-xs-4" for="email">Email</label><input class="col-lg-8 col-md-8 col-sm-8 col-xs-8" type="text" id="email" name="email" value="'.stripslashes($email).'" tabindex="2" /><br></div>
-				<div class="input-group col-lg-12 contact-form">
-				<input class="col-lg-4 col-lg-offset-8 col-md-4 col-md-offset-8 col-sm-6 col-sm-offset-6 col-xs-6 right" type="submit" name="envoi" value="Envoyer le formulaire" /><br/><br/><br/><br/></div>
+		<h1 class="text-center">Mot de passe oublié ?</h1>
+            <p class="text-center">Veuillez renseigner l\'adresse mail associée à votre compte</p><br/>
+				
+				<div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-5 text-right"><h2><label for="email">Email</label>
+                        </h2></div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-7 text-left"><input type="text" id="email" name="email" value="' . stripslashes($email) . '" tabindex="2" />
+                    </div>
+                </div>				
+				<input type="submit" name="envoi" value="Envoyer" />
 		</form>
-					</div>
-		<div class="col-lg-4 col-md-4 hidden-sm hidden-xs" id="droite">
-		</div>
 	';
-}
-?>
-<?php include('footer.php');?>
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php include('footer.php'); ?>
