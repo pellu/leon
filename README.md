39ba9f28fdbe3585505a7a9598203f3c0f72bbe0
pellu <jordanpelluard@gmail.com>
2016-07-05 21:33:29 +0200

    modifier annonce ok

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Site réalisé lors d'une compétition de l'IESA Multimédia par l'équipe Leon
 
 Les fichiers config.php ne sont pas présents sur git
+
+Le site est disponible sur letsplayce.com et la landing sur letsplayce.com/landing
--- website/modifierannonce.php
+++ website/modifierannonce.php
@@ -15,6 +15,80 @@ if (isset($_SESSION['email'])) {
 
                     <h1>Modifier mon annonce</h1>
                     <?php
+                    // SCRIPT ENVOI PHOTO
+                        define('TARGET', '../website/photos/');    // Repertoire cible
+                        define('MAX_SIZE', 100000000);    // Taille max en octets du fichier
+                        define('WIDTH_MAX', 2000);    // Largeur max de l'image en pixels
+                        define('HEIGHT_MAX', 2000);    // Hauteur max de l'image en pixels
+
+                        // Tableaux de donnees
+                        $tabExt = array('jpg', 'gif', 'png', 'jpeg');    // Extensions autorisees
+                        $infosImg = array();
+
+                        // Variables
+                        $extension = '';
+                        $message = '';
+                        $nomImage = '';
+
+                        /************************************************************
+                         * Creation du repertoire cible si inexistant
+                         *************************************************************/
+                        if (!is_dir(TARGET)) {
+                            if (!mkdir(TARGET, 0755)) {
+                                exit('Erreur : le r&eacute;pertoire cible ne peut-être cr&eacute;&eacute; ! V&eacute;rifiez que vous diposiez des droits suffisants pour le faire ou cr&eacute;ez le manuellement !');
+                            }
+                        }
+
+                        if (!empty($_POST)) {
+                            // On verifie si le champ est rempli
+                            if (!empty($_FILES['photo']['name'])) {
+                                // Recuperation de l'extension du fichier
+                                $extension = pathinfo($_FILES['photo']['name'], PATHINFO_EXTENSION);
+
+                                // On verifie l'extension du fichier
+                                if (in_array(strtolower($extension), $tabExt)) {
+                                    // On recupere les dimensions du fichier
+                                    $infosImg = getimagesize($_FILES['photo']['tmp_name']);
+
+                                    // On verifie le type de l'image
+                                    if ($infosImg[2] >= 1 && $infosImg[2] <= 14) {
+                                        // On verifie les dimensions et taille de l'image
+                                        if (($infosImg[0] <= WIDTH_MAX) && ($infosImg[1] <= HEIGHT_MAX) && (filesize($_FILES['photo']['tmp_name']) <= MAX_SIZE)) {
+                                            // Parcours du tableau d'erreurs
+                                            if (isset($_FILES['photo']['error'])
+                                                && UPLOAD_ERR_OK === $_FILES['photo']['error']
+                                            ) {
+                                                // On renomme le fichier
+                                                $nomImage = md5(uniqid()) . '.' . $extension;
+
+                                                // Si c'est OK, on teste l'upload
+                                                if (move_uploaded_file($_FILES['photo']['tmp_name'], TARGET . $nomImage)) {
+                                                    $message = '<p>Upload r&eacute;ussi !</p>';
+                                                } else {
+                                                    // Sinon on affiche une erreur systeme
+                                                    $message = '<p>Probl&egrave;me lors de l\'upload !</p>';
+                                                }
+                                            } else {
+                                                $message = '<p>Une erreur interne a empêch&eacute; l\'uplaod de l\'image</p>';
+                                            }
+                                        } else {
+                                            // Sinon erreur sur les dimensions et taille de l'image
+                                            $message = '<p>Erreur dans les dimensions de l\'image !</p>';
+                                        }
+                                    } else {
+                                        // Sinon erreur sur le type de l'image
+                                        $message = '<p>Le fichier à uploader n\'est pas une image !</p>';
+                                    }
+                                } else {
+                                    // Sinon on affiche une erreur pour l'extension
+                                    $message = '<p>L\'extension du fichier est incorrecte !</p>';
+                                }
+                            } else {
+                                // Sinon on affiche une erreur pour le champ vide
+                                $message = '<p>Veuillez remplir le formulaire svp !</p>';
+                            }
+                        }
+
                     $id_news = $_GET["id"];
                     $req = $mysql->prepare('SELECT c.* , p.* FROM news c, profil p WHERE c.pseudo_news=p.id AND id_news="' . $id_news . '" ORDER BY c.id_news DESC LIMIT 1');
                     $req->execute(array($_GET['id']));
@@ -22,19 +96,25 @@ if (isset($_SESSION['email'])) {
                         ?>
                         <?php
                         if (isset($_POST['contenu_news'])) {
-                            $contenu_news = $_POST['contenu_news'];
+                            $contenu_news = nl2br($_POST['contenu_news']);
                         } else {
-                            $contenu_news = $donnees['contenu_news'];
+                            $contenu_news = str_replace('<br />', "\n", $donnees['contenu_news']);
+                        }
+                        if (isset($nomImage)) {
+                            $photo = $nomImage;
+                        } else {
+                            $photo = $donnees['photo'];
                         }
                         if (isset($_POST['submit'])) {
                             $date = date("d-m-Y");
                             $id_news = $_GET["id"];
 
-                            $modif = $mysql->prepare("UPDATE news SET contenu_news='$contenu_news', date_news='$date' WHERE id_news='" . $id_news . "'");
-                            $modif->execute(array(
-                                'contenu_news' => $contenu_news,
-                                'date' => $date
-                            ));
+                            $modif = $mysql->prepare("UPDATE news SET contenu_news='$contenu_news', date_news='$date', photo='$photo' WHERE id_news='" . $id_news . "'");
+                            $modif->bindParam(':contenu_news', $contenu_news);
+                            $modif->bindParam(':date', $date);
+                            $modif->bindParam(':photo', $photo);
+                            $modif->execute();
+                            
                             echo '<h2>Modification effective</h2>';
                         } else {
                             echo '';
@@ -46,13 +126,6 @@ if (isset($_SESSION['email'])) {
                                 </h2></div>
                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-left">
                                 <h2><?php echo $donnees['titre_news']; ?></h2>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-right"><h2><label>Pseudo de l'hébergeur : </label>
-                                </h2></div>
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-left">
-                                <h2><?php echo $donnees['pseudo']; ?></h2>
                             </div>
                         </div>
                         <div class="row">
@@ -103,30 +176,35 @@ if (isset($_SESSION['email'])) {
                                 <h2><?php echo  $donnees['animaux']; ?></h2>
                             </div>
                         </div>
-                        <form method="post" action="">
+                        <form method="post" action="" enctype="multipart/form-data">
                             <div class="row">
                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-right"><h2><label
                                             for="pseudo">Description de l'évènement : </label>
                                     </h2></div>
                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-left"><textarea
-                                        name="contenu_news"/><?php echo $contenu_news; ?></textarea>
+                                        name="contenu_news" id="annonce-description" /><?php echo $contenu_news; ?></textarea>
                                 </div>
                             </div>
+                                <p>Il vous reste <span id="reste_description"></span> caractères.</p>
+                                    <script type="text/javascript">
+                                        <!--
+                                            maxlength_textarea('annonce-description','reste_description',800);
+                                        -->
+                                   </script>
                             <div class="row">
-                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-right"><h2><label
-                                            for="pseudo">Photo<a href="#" data-toggle="tooltip" data-placement="top"
-                                                                 title="Photo 1200x1200px maximum">*</a> de l'évènement : </label>
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-right"><h2><label for="pseudo">Photo<a href="#" data-toggle="tooltip" data-placement="top" title="Photo 1200x1200px maximum">*</a> de l'évènement : </label>
                                     </h2></div>
                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-left"><div class="file-input" style="margin-left:0; max-width: 163px;">Choisissez votre image
-                                        <input name="photo" type="file" id="fichier_a_uploader" accept="image/*"
-                                               onchange="loadFile(event)"/>
+                                        <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo MAX_SIZE; ?>"/>
+                                        <input name="photo" type="file" id="fichier_a_uploader" accept="image/*" onchange="loadFile(event)"/>
+                                        <img width="140px;" height="88" src="http://localhost/leon/website/photos/<?php echo $donnees['photo']; ?>">
                                     </div>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-right"><h2><label>Aperçu de la Photo</label>
                                     </h2></div>
-                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-left"><img id="output" href="" title="Votre image"/>
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-left"><p><img id="output" href="" title="Votre image actuelle"/></p>
                                     <style>#output{height: 105px;width: 167px;}</style>
                                     <script>
                                         var loadFile = function(event) {
